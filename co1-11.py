e9c8540cc6872e001b90e810125c7a653d3c716c
ayishafida <ayishashraf7814@gmail.com>
2021-03-17 18:12:28 +0530

    committed co1-11

---
+++ co1-11.py
@@ -0,0 +1,11 @@
+a = int(input("Enter 1st number"))
+b = int(input("Enter 2nd number"))
+c = int(input("Enter 3rd number"))
+if a > b:
+    if a > c:
+        print("a is largest")
+elif b > c:
+    if b > a:
+        print("b is largest")
+else:
+    print("c is largest")
