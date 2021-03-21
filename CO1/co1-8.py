90a08defc322b9c48157dc43dc171522cbc7355a
ayishafida <ayishashraf7814@gmail.com>
2021-03-21 14:39:02 +0530

    committed co2-8

--- CO1/co1-8.py
+++ CO1/co1-8.py
@@ -1,5 +1,6 @@
-string = input("enter string")
-l = []
-s = string.split(" ")
-l.append(s)
-print(l)
+s = input("Enter a string:")
+b = s[0]
+for i in s:
+    if i == b:
+        s = s.replace(i, "$")
+print(b + s[1:])
