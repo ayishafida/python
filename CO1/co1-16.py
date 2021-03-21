7d13039417339e0e51a7a169ec14f898df7968b9
ayishafida <ayishashraf7814@gmail.com>
2021-03-21 15:46:27 +0530

    committed 2.txt

--- CO1/co1-16.py
+++ CO1/co1-16.py
@@ -4,4 +4,4 @@ w1 = s1[0]
 w2 = s2[0]
 a1 = w2+s1[1:]
 a2 = w1+s2[1:]
-print("the swapped string is:", a1+" "+a2)
+print("The swapped string is:", a1+" "+a2)
--- CO2/co2-4.py
+++ CO2/co2-4.py
@@ -1,22 +1,12 @@
 import math
-perfect = []
-start = int(input("Enter starting range:"))
-end = int(input("Enter ending range:"))
-for i in range(start, end+1):
-    flag = 0
-    num = i
-    while num > 0:
-        digit = num % 10
-        if digit not in [0, 2, 4, 6, 8]:
-            flag = 1
-            break
-        num = int(num / 10)
-    if flag == 0 and int(math.sqrt(i)+0.5)**2 == i:
-        perfect.append(i)
-print("The list of perfect squares are", perfect)
-# if start % 10 and not(start / 10000) and start / 1000 and start / 100 and start / 10:
-#     if end % 10 and not(end / 10000) and end / 1000 and end / 100 and end / 10:
-#     else:
-#         print("Ending range not in 4 digits")
-# else:
-#     print("Starting range not in 4 digits")
+for i in range(1000,10000):
+    n = int(math.sqrt(i))
+    if n * n == i:
+        s = i
+        while s != 0:
+            r = s % 10
+            s = s // 10
+            if r % 2 != 0:
+                break
+            else:
+                print(i)
