cd27cc088020921acd2a2e500d8d609dab4cd368
ayishafida <ayishashraf7814@gmail.com>
2021-03-25 00:30:34 +0530

    committed 2.txt

---
+++ CO4/co4-3.py
@@ -0,0 +1,22 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.__length = length
+        self.__width = width
+
+    def area(self):
+        return self.__length * self.__width
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+l1 = int(input("Enter the length of first rectangle:"))
+w1 = int(input("Enter the length of first rectangle:"))
+rectangle1 = (l1, w1)
+l2 = int(input("Enter the length of second rectangle:"))
+w2 = int(input("Enter the length of second rectangle:"))
+rectangle2 = (l2, w2)
+if rectangle1 < rectangle2:
+    print("Area of rectangle1 is smaller")
+else:
+    print("Area of rectangle 2 is smaller")
+
---
+++ CO5/co5-3.py
@@ -0,0 +1,18 @@
+import csv
+
+l = []
+with open("city.csv", "w")as file:
+    write = csv.writer(file)
+    write.writerow(["id", "place", "district"])
+    limit = int(input("Enter the lines of data you want to enter:"))
+    for i in range(limit):
+        row_string = input("Enter data (id,place,district) separated by comma:")
+        row_list = row_string.split(",")
+        write.writerow(row_list)
+    with open("city.csv", "r") as file:
+        read = csv.reader(file)
+        for row in read:
+            print(row)
+            l.append(",".join(row))
+
+print(l)
